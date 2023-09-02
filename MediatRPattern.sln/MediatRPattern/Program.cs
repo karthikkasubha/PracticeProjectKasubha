40a05f60ba08ae4a2e1995a1d071f163c4cd8a78
Karthik Kasubha <karthik.kasubha@datacom.co.nz>
2023-09-03 11:58:59 +1200

    1158  commit

--- MediatRPattern.sln/MediatRPattern/Program.cs
+++ MediatRPattern.sln/MediatRPattern/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");  //test test again
+Console.WriteLine("Hello, World!");  //test test again  1158 commit
